e437c21f70ec39615053822a79cbbd02e824684c
RociOrtega <rocio.ortega@ug.uchile.cl>
2022-04-06 12:21:24 -0400

    dinamic style and name

--- src/components/Metronome/textDinamic/DinamicDescription.jsx
+++ src/components/Metronome/textDinamic/DinamicDescription.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { dinamics } from "./dinamics";
+import './dinamicTextStyle.css'
 
-const Text = (props) => {
+const DinamicDescription = (props) => {
     const[description, setDescription] = useState("Allegro - Presto")
     const bpm = props.bpm;
     useEffect(() => {
         dinamics.map((item) => {
-            return (bpm >= item.bpmMin && bpm <= item.bpmMax) ? setDescription(item.description) : null
+            return (bpm >= item.bpmMin && bpm <= item.bpmMax) ? setDescription(item.name) : null
         })
     }, [bpm])
     return(
-        <p className="metro-text">{description}</p>
+        <>
+            <p className="metro-text">{description}</p>
+        </>
     )
 }
 
-export default Text;
+export default DinamicDescription;
